5c4290da9794a584dce149334c639f54c1e3042f
“viettri258” <viettri258@gmail.com>
2018-05-04 16:13:07 +0700

    dev

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
 <?php
-namespace irt\database;
+require_once __DIR__ . '/../../../autoload.php'; // Autoload files using Composer autoload
+
+use irt\database\example\DBO;
+
+$dbo = new DBO;
+$result = $dbo->demoDB()->getCollection();
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
 ?>
